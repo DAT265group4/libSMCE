d3715f2b985656dafc92c366f086e36bb7014e22
kianaadib <77792124+kianaadib@users.noreply.github.com>
2021-12-01 15:16:09 +0100

    add test for remove file from sd- Incomplete

--- test/SD.cpp
+++ test/SD.cpp
@@ -1,4 +1,72 @@
 //
-// Created by ASUS on 12/1/2021.
+// Created by ASUS on 11/18/2021.
 //
+#include <iostream>
+#include <catch2/catch_test_macros.hpp>
+//#include "Ardrivo/SD.h"
+#include "defs.hpp"
+
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <catch2/catch_test_macros.hpp>
+#include "SMCE/Board.hpp"
+#include "SMCE/BoardConf.hpp"
+#include "SMCE/BoardView.hpp"
+#include "SMCE/Toolchain.hpp"
+#include "defs.hpp"
+
+using namespace std::literals;
+
+TEST_CASE("t1", "[SD]") {
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("SD remove file", "[SD File]") {
+    smce::Toolchain tc{SMCE_PATH};
+    REQUIRE(!tc.check_suitable_environment());
+    smce::Sketch sk{SKETCHES_PATH "T2", {
+            .fqbn = "arduino:avr:nano",
+            .legacy_preproc_libs = { {"SD"} }
+    }};
+    const auto ec = tc.compile(sk);
+    if (ec)
+        std::cerr << ec.message() << '\n' << tc.build_log().second << std::endl;
+    REQUIRE_FALSE(ec);
+
+    smce::Board br{};
+    smce::BoardConfig bc{
+            /* .pins = */{0},
+            /* .gpio_drivers = */{ smce::BoardConfig::GpioDrivers{0, smce::BoardConfig::GpioDrivers::DigitalDriver{true, true}, std::nullopt} },
+            {},
+            /* .sd_cards = */{ smce::BoardConfig::SecureDigitalStorage{0, STORAGE_PATH } },
+            {}
+    };
+    REQUIRE(br.configure(std::move(bc)));
+
+   /* if (std::filesystem::exists(STORAGE_PATH))
+        std::filesystem::remove_all(STORAGE_PATH);
+    std::filesystem::create_directory(STORAGE_PATH);*/
+    REQUIRE(br.attach_sketch(sk));
+    REQUIRE(br.start());
+    auto d0 = br.view().pins[0].digital();
+    test_pin_delayable(d0, true, 16384, 1ms); // wait for the pin to be set
+    REQUIRE(br.stop());
+    std::cerr << br.runtime_log().second << std::endl;
+
+   /* REQUIRE(std::filesystem::exists(STORAGE_PATH "foo"));
+    REQUIRE(std::filesystem::is_directory(STORAGE_PATH "foo"));
+    REQUIRE(std::filesystem::exists(STORAGE_PATH "bar"));
+    REQUIRE(std::filesystem::is_directory(STORAGE_PATH "bar"));*/
+    REQUIRE_FALSE(std::filesystem::exists(STORAGE_PATH "bar/baz"));
+    //REQUIRE(std::filesystem::is_regular_file(STORAGE_PATH "bar/baz"));
+    //std::ifstream baz{STORAGE_PATH "bar/baz"};
+    /*REQUIRE(baz.is_open());
+    std::string s;
+    baz >> s;
+    REQUIRE(s == "quxx");*/
+
+}
+
 
---
+++ test/sketches/T2/T2.ino
@@ -0,0 +1,19 @@
+#include <SD.h>
+
+void setup() {
+    SD.begin();
+    SD.mkdir("/foo");
+    SD.mkdir("bar");
+    File f = SD.open("bar/baz", FILE_WRITE);
+    f.print("quxx");
+    f.close();
+    pinMode(0, OUTPUT);
+    digitalWrite(0, HIGH);
+
+    SD.remove("bar/baz")
+
+}
+
+void loop() {
+    delay(1);
+}
