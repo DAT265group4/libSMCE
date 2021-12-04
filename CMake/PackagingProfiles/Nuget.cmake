5410f198f33209b01bd923d4b5a021746742e7ec
AtiyeEtemadi <atiye@student.chalmers.se>
2021-12-04 16:31:37 +0100

    Last nuget update

---
+++ CMake/PackagingProfiles/Nuget.cmake
@@ -0,0 +1,23 @@
+#
+#  PackagingProfiles/Default.cmake
+#  Copyright 2021 ItJustWorksTM
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# Check ReadMe to know how to create Nuget package
+set (CPACK_GENERATOR NuGet)
+set (CPACK_NUGET_PACKAGE_NAME  "${PROJECT_NAME}")
+set (CPACK_NUGET_PACKAGE_VERSION  "${PROJECT_VERSION}")
+set (CPACK_NUGET_PACKAGE_AUTHORS "ItJustWorksTM")
+set(CPACK_NUGET_PACKAGE_DESCRIPTION "This package is the spiritual successor application to the backend of SMartCarEmul application.")
